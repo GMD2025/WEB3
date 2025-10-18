3678434f86235739792b701627726f7f0f68b9f9
MichaelLeo-code <michael.leo.dk@gmail.com>
2025-10-18 21:07:09 +0200

    config fixes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 // import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        // vueDevTools(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-            '@domain': fileURLToPath(new URL('../domain/src', import.meta.url))
-        },
+  plugins: [
+    vue(),
+    // vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@domain": fileURLToPath(new URL("../domain/src", import.meta.url)),
     },
-})
+  },
+  worker: {
+    format: "es",
+  },
+  optimizeDeps: {
+    exclude: ["@domain"],
+  },
+});
